2f14123e12c1850c0ce93c789f72f6cd2ae813b2
Cornea Adrian Emilian <emilian.cornea@yahoo.com>
2021-10-29 15:31:58 +0300

    small fix

--- MiniTetris/MiniTetris.cpp
+++ MiniTetris/MiniTetris.cpp
@@ -11,13 +11,13 @@ int main()
 	std::cout << "Insert the rows and columns of the object";
 	std::cin >> obj_rows >> obj_columns;
 	std::cout << "Create the object using * as part of it and other character for space";
-	std::cout << "Please insert characters within the limits you decided: Rows " << a << " Columns " << b <<std::endl;
+	std::cout << "Please insert characters within the limits you decided: Rows " << obj_rows << " Columns " << obj_columns <<std::endl;
 	tetris->createObj(obj_rows, obj_columns);
 	std::cout << "Insert the rows and columns of the Map";
 	std::cin >> map_rows >> map_columns;
 	tetris->createMap(map_rows, map_columns);
 	std::cout << "Create the Map using . as a free to use space and # as used area";
-	std::cout << "Please insert characters within the limits you decided: Rows " << c << " Columns " << d << std::endl;
+	std::cout << "Please insert characters within the limits you decided: Rows " << map_rows << " Columns " << map_columns << std::endl;
 	tetris->objRls(obj_columns);
 	tetris->spaceFinder(map_columns);
 	std::cout << tetris->getRez();
